85a5ad260590d731d12c77e8016253656a1e4a9b
[Abdulwaasi Saliu] <[wasiu.saliu@miva.edu.ng]>
2026-05-07 22:21:44 +0100

    mail fix

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { uploads } = require('../utils/cloudinary');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/signup', authController.register);
 router.post('/login', authController.login);
@@ -9,6 +10,6 @@ router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password/:token', authController.resetPassword);
 router.post('/verify-otp', authController.verifyOTP);
 router.post('/resend-otp', authController.resendOTP);
-router.post('/save-diagnosis', uploads.single('image'), authController.saveDiagnosis);
+router.post('/save-diagnosis', protect, uploads.single('image'), authController.saveDiagnosis);
  
 module.exports = router;
--- backend/src/utils/sendEmails.js
+++ backend/src/utils/sendEmails.js
@@ -22,14 +22,18 @@ const sendEmail = async (options) => {
 
 const sendOTPEmail = async (email, otp) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
-// Define your frontend URL (your Vercel link)
   const verifyUrl = `https://agro-health-chi.vercel.app/verify-otp?email=${email}`;
 
   const mailOptions = {
