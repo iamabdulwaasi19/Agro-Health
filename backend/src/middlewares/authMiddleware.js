98fe2cb8f721394a64e3723e413a4e6fe7b70309
[Abdulwaasi Saliu] <[wasiu.saliu@miva.edu.ng]>
2026-05-08 11:27:38 +0100

    New commitment

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const protect = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
   const token = req.header('Authorization')?.split(' ')[1];
   if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
 
@@ -13,4 +13,4 @@ const protect = (req, res, next) => {
   }
 };
 
-module.exports = { protect };
+module.exports = { authMiddleware };
