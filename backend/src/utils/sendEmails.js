9695d879eb3bedc941b9ea483ed594d2babe25e4
[Abdulwaasi Saliu] <[wasiu.saliu@miva.edu.ng]>
2026-05-08 17:05:53 +0100

    debugging

--- backend/src/utils/sendEmails.js
+++ backend/src/utils/sendEmails.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: console.log(process.env.EMAIL_USER);,
-    pass: console.log(process.env.EMAIL_PASS);,
+    user: console.log(process.env.EMAIL_USER),
+    pass: console.log(process.env.EMAIL_PASS),
     // user: process.env.EMAIL_USER,
     // pass: process.env.EMAIL_PASS,
   },
